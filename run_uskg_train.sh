e11dbcccf9a00b5c4c5b8c5ace958b5a46d9013c
sythello <sythello@163.com>
2022-03-07 16:06:03 -0800

    add my cfg (A-...) for fine-tuning exp on original spider

---
+++ run_uskg_train.sh
@@ -0,0 +1,31 @@
+python train.py \
+--seed 2 \
+--cfg Salesforce/A-T5_base_finetune_spider_with_cell_value.cfg \
+--run_name A-T5_base_finetune_spider_with_cell_value \
+--logging_strategy steps \
+--logging_first_step true \
+--logging_steps 4 \
+--evaluation_strategy steps \
+--eval_steps 500 \
+--metric_for_best_model avr \
+--greater_is_better true \
+--save_strategy steps \
+--save_steps 500 \
+--save_total_limit 1 \
+--load_best_model_at_end \
+--gradient_accumulation_steps 8 \
+--num_train_epochs 400 \
+--adafactor true \
+--learning_rate 5e-5 \
+--do_train \
+--do_eval \
+--do_predict \
+--predict_with_generate \
+--output_dir output/A-T5_base_finetune_spider_with_cell_value \
+--overwrite_output_dir \
+--per_device_train_batch_size 4 \
+--per_device_eval_batch_size 16 \
+--generation_num_beams 4 \
+--generation_max_length 128 \
+--input_max_length 1024 \
+--ddp_find_unused_parameters true
