4995a456b91484d5ae37cb83d9b7930babc7578a
sythello <yshao@eng.ucsd.edu>
2023-11-20 18:45:18 -0800

    add config for gpt2-medium pfx=20, shorter seq_len

---
+++ sdra_training_scripts/run_uskg_gpt2_medium_pfx20_train.sh
@@ -0,0 +1,59 @@
+RUN_NAME=A-GPT2_medium_prefix_spider_with_cell_value
+RUN_ID=20231113
+
+mkdir -p output/$RUN_NAME
+
+python train.py \
+--seed 2 \
+--cfg Salesforce/A-GPT2_medium_prefix_spider_with_cell_value-pfx=20.cfg \
+--run_name $RUN_NAME \
+--logging_strategy steps \
+--logging_first_step true \
+--logging_steps 4 \
+--evaluation_strategy steps \
+--eval_steps 500 \
+--metric_for_best_model avr \
+--save_strategy steps \
+--save_steps 500 \
+--save_total_limit 2 \
+--overwrite_output_dir \
+--load_best_model_at_end \
+--greater_is_better true \
+--gradient_accumulation_steps 32 \
+--num_train_epochs 400 \
+--adafactor true \
+--learning_rate 5e-5 \
+--do_train \
+--do_eval \
+--do_predict \
+--predict_with_generate \
+--output_dir output/$RUN_NAME/run-$RUN_ID \
+--per_device_train_batch_size 2 \
+--per_device_eval_batch_size 4 \
+--generation_num_beams 1 \
+--generation_max_length 490 \
+--input_max_length 362 \
+--ddp_find_unused_parameters true \
+--is_causal_lm true
+
+
+## Back-up Args
+# bs = 2: works (M~=9700M; T~=140h)
+# bs = 1: works (M~=5500M; T also ~140h?)
+
+# Try using smaller seq length (same as gpt2-small) to make it fit into memory
+
+
+
+# Can't use `tee output/$RUN_NAME/training-log.txt`: tee interferes with input()
+
+## According to paper appendix: input_max_length=512, batch_size=32, beam_size=1
+## Notice: generation_max_length need to be the length of "generated seq"; for gpt2, it's input + output
+## Now using input_max_length = 372, generation_max_length = 500
+## Now input_max_length only stands for "input", not concatenated sequence
+
+# About greater_is_better: if using loss, lower is better. Provided is using 'avr' which is SQL eval
+# Set save_total_limit 2 to save the last ckpt, just in case
+
+# Added --is_causal_lm true to also provide info for trainer post-processing
+# (It is also specified in model_args)
